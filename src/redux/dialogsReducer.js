e95b857dbc60f3cc63e99312c3372d083d19f7ab
Igor <igoralyanoy@gmail.com>
2021-06-21 11:37:16 +0300

    refactor: move object from argument to variable

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,44 +1,43 @@
 const SEND_MESSAGE = "SEND-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
-const dialogsReducer = (
-  state = {
-    dialogsData: [
-      {
-        id: 1,
-        img: "https://randomuser.me/api/portraits/women/81.jpg",
-        name: "Maria",
-      },
-      {
-        id: 2,
-        img: "https://randomuser.me/api/portraits/men/64.jpg",
-        name: "Alex",
-      },
-      {
-        id: 3,
-        img: "https://randomuser.me/api/portraits/women/4.jpg",
-        name: "Karina",
-      },
-      {
-        id: 4,
-        img: "https://randomuser.me/api/portraits/men/55.jpg",
-        name: "Victor",
-      },
-      {
-        id: 5,
-        img: "https://randomuser.me/api/portraits/women/64.jpg",
-        name: "Ann",
-      },
-    ],
-    messagesData: [
-      { id: 1, message: "Hello!" },
-      { id: 2, message: "How are you?" },
-      { id: 3, message: "Hey nice weather today isn't it?" },
-    ],
-    newMessageText: "",
-  },
-  action
-) => {
+const initialState = {
+  dialogsData: [
+    {
+      id: 1,
+      img: "https://randomuser.me/api/portraits/women/81.jpg",
+      name: "Maria",
+    },
+    {
+      id: 2,
+      img: "https://randomuser.me/api/portraits/men/64.jpg",
+      name: "Alex",
+    },
+    {
+      id: 3,
+      img: "https://randomuser.me/api/portraits/women/4.jpg",
+      name: "Karina",
+    },
+    {
+      id: 4,
+      img: "https://randomuser.me/api/portraits/men/55.jpg",
+      name: "Victor",
+    },
+    {
+      id: 5,
+      img: "https://randomuser.me/api/portraits/women/64.jpg",
+      name: "Ann",
+    },
+  ],
+  messagesData: [
+    { id: 1, message: "Hello!" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Hey nice weather today isn't it?" },
+  ],
+  newMessageText: "",
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case SEND_MESSAGE:
       let newMessage = {
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,16 +1,15 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (
-  state = {
-    postsData: [
-      { id: 1, message: "Post 1", likes: "5" },
-      { id: 2, message: "Post 2", likes: "3" },
-    ],
-    newPostText: "",
-  },
-  action
-) => {
+const initialState = {
+  postsData: [
+    { id: 1, message: "Post 1", likes: "5" },
+    { id: 2, message: "Post 2", likes: "3" },
+  ],
+  newPostText: "",
+};
+
+const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let newPost = {
