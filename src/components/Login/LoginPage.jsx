eefb1d36f4424b64effce328f555af533085e772
Igor <igoralyanoy@gmail.com>
2021-08-08 13:33:46 +0300

    change login form by elements of formik

--- src/components/Login/LoginPage.jsx
+++ src/components/Login/LoginPage.jsx
@@ -1,14 +1,5 @@
 import React from "react";
-import { useFormik } from "formik";
-
-const LoginPage = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <LoginForm />
-    </div>
-  );
-};
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const LoginForm = () => {
   let initialValues = {
@@ -23,7 +14,6 @@ const LoginForm = () => {
 
   let validate = (values) => {
     let errors = {};
-
     if (!values.login) {
       errors.login = "Required";
     }
@@ -33,57 +23,41 @@ const LoginForm = () => {
     return errors;
   };
 
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validate,
-  }); 
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div>
-        <label htmlFor="login">Login</label> <br />
-        <input
-          type="login"
-          id="login"
-          name="login"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.login}
-        />
-        {formik.touched.login && formik.errors.login ? (
-          <span style={{ color: "red" }}>{formik.errors.login}</span>
-        ) : null}
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <br />
-        <input
-          type="password"
-          id="password"
-          name="password"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-        {formik.touched.password && formik.errors.password ? (
-          <span style={{ color: "red" }}>{formik.errors.password}</span>
-        ) : null}
-      </div>
-      <div>
-        <input
-          type="checkbox"
-          id="checkbox"
-          name="checkbox"
-          onChange={formik.handleChange}
-          value={formik.values.checkbox}
-        />
-        <label htmlFor="checkbox">Remeber me</label>
-      </div>
-      <div>
-        <button type="submit">Log in</button>
-      </div>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div>
+          <label htmlFor="login">Login</label> <br />
+          <Field type="login" id="login" name="login" />
+          <ErrorMessage name="login" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <br />
+          <Field type="password" id="password" name="password" />
+          <ErrorMessage name="password" />
+        </div>
+        <div>
+          <Field type="checkbox" id="checkbox" name="checkbox" />
+          <label htmlFor="checkbox">Remeber me</label>
+        </div>
+        <div>
+          <button type="submit">Log in</button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
   );
 };
 export default LoginPage;
